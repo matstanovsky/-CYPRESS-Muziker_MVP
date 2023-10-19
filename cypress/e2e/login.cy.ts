e6108485eb2c3247f486e9b6528bc7f0c003adaf
Matus Stanovsky <matus.stanobvsky@gmail.com>
2023-10-19 14:51:53 +0200

    [login-tests] test: add tests for login page

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,11 @@
 import { userEmailInput, userPasswordInput } from "../support/pages/landingPage";
-import { loginButton } from "../support/pages/loginPage";
+import { RANDOM_USER_EMAIL, REAL_USER_EMAIL, REAL_USER_PASSWORD, createAccount, forgetPasswordLink, loginButton } from "../support/pages/loginPage";
 
 describe('login page tests', ()=>{
-    beforeEach(()=>{cy.visit('/login')});
+    beforeEach(()=>{
+        cy.setCookiesConsent();
+        cy.visit('/login');
+    });
 
     it('login page contains fields for credentials and submit button', ()=> {
         userEmailInput()
@@ -19,7 +22,63 @@ describe('login page tests', ()=>{
             .should('be.visible'); 
     });
 
-    it('random credentials do not login user', ()=> {
-        userEmailInput().type('myRandom@email.intheworld');
+    it('random credentials do not log user and show alert', ()=> {
+        userEmailInput().type(RANDOM_USER_EMAIL);
+        userPasswordInput().type('my1RandomPassword!');
+        loginButton().click();
+
+        cy.get('.alert').contains("Prihlasovacie údaje sú nesprávne.")
+            .should('exist')
+            .should('be.visible');
+        userEmailInput()
+            .should('have.value',RANDOM_USER_EMAIL);        
+    });
+
+    it('Log real user show confirmation message', ()=> {
+        userEmailInput().type(REAL_USER_EMAIL);
+        userPasswordInput().type(REAL_USER_PASSWORD);
+        loginButton().click();
+        
+        cy.get('.alert').contains("Úspešne prihlásený")
+            .should('exist')
+            .should('be.visible');
+    });
+
+    it('Create account button is available on login page and navigates to correct page', ()=> {
+        createAccount()
+            .should('exist')
+            .should('be.visible')
+            .click();
+
+        cy.contains("Nový účet Muziker Smile")
+            .should('exist')
+            .should('be.visible');
+
+        userEmailInput()
+            .should('exist')
+            .should('be.visible');
+
+        userPasswordInput()
+            .should('exist')
+            .should('be.visible');
+    });
+
+    it('Forget password link works and navigates to correct page', ()=> {
+        forgetPasswordLink()
+            .should('exist')
+            .should('be.visible')
+            .click();
+        
+        userEmailInput()
+            .should('exist')
+            .should('be.visible');
+
+        cy.contains('Problém s heslom?')
+            .should('exist')
+            .should('be.visible');
+        
+        cy.contains('Pošlite mi e-mail na obnovu hesla')
+            .should('exist')
+            .should('be.visible');
     })
 })
--- cypress/support/pages/loginPage.ts
+++ cypress/support/pages/loginPage.ts
@@ -1,1 +1,7 @@
-export const loginButton = () => {return cy.get('[type="submit"][value="Prihlásiť"]').click()}
+export const RANDOM_USER_EMAIL: string = "myRandom@email.intheworld";
+export const REAL_USER_EMAIL: string = "thisismojsen@gmail.com";
+export const REAL_USER_PASSWORD: string = "Pivo123!";
+
+export const loginButton = () => {return cy.get('[type="submit"][value="Prihlásiť"]');}
+export const createAccount = () => {return cy.get('*[class*="mzkr-btn-primary"]').contains("Vytvoriť účet");}
+export const forgetPasswordLink = () => {return cy.get('a').contains("Neviem svoje heslo");}
